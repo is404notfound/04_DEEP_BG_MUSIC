82dc99f3b22c9be6949bf5b3ddadfa1ff4d00b9e
Jiyoon Lee <easyoon@wemakeprice.com>
2022-10-05 12:04:22 +0900

    Set Initial constructure

---
+++ src/layouts/App.js
@@ -0,0 +1,18 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          DEEP_BG_MUSIC
+        </p>
+      </header>
+    </div>
+  );
+}
+<script>
+
+
+</script>
+export default App;
